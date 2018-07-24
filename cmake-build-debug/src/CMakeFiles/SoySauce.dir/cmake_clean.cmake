02874fd8df6a640a895b9cb96a03dbf9021e3e28
wangnick2017 <wangnick2017@outlook.com>
2018-07-24 11:07:21 +0800

    init files

---
+++ cmake-build-debug/src/CMakeFiles/SoySauce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoySauce.dir/main.cpp.o"
+  "SoySauce.pdb"
+  "SoySauce"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SoySauce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
